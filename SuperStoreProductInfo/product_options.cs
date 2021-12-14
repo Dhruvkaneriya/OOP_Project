450d05a2c4d29f077257647b3c0c8ac75a4f268b
DhruvKaneriya <32631492+Dhruvkaneriya@users.noreply.github.com>
2021-12-14 14:51:45 -0500

    commit

--- SuperStoreProductInfo/product_options.cs
+++ SuperStoreProductInfo/product_options.cs
@@ -6,8 +6,24 @@ using System.Threading.Tasks;
 
 namespace SuperStoreProductInfo
 {
-    class product_options
+    public class product_options
     {
+            public int ID { get; set; }
+            public string Product_Category { get; set; }
+            public string Product_Name { get; set; }
+            public string Brand { get; set; }
+            public float Price { get; set; }
+            public double Barcode { get; set; }
 
+            // public constructor
+            public product_options(int id ,string product_category, string product_name, string brand, float price, double barcode)
+            {
+                ID = id;
+                Product_Category = product_category;
+                Product_Name = product_name;
+                Brand = brand;
+                Price = price;
+                Barcode = barcode;
+            }
     }
 }
